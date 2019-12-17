80355f4f83ea3c0cc3104b7f065571ac2dfafe34
dammy_abayomi <sweetmumolu>
2019-12-17 00:18:49 +0000

    Code formatted.

--- test/num_extension_test.dart
+++ test/num_extension_test.dart
@@ -5,9 +5,7 @@ import 'package:num_extension/src/num_extension.dart';
 void main() {
 
   group('NumExtension', () {
-
     group('Nums', () {
-
       test('Can perform addition operation on 2 integers', () {
         expect(1.plus(3), 4);
       });
@@ -75,35 +73,36 @@ void main() {
       });
     });
 
-
     group('List<num>', () {
-
-      test('Can calculate the sum of elements in a `List`', (){
-        expect([2,3,5,-4].sum, 6);
+      test('Can calculate the sum of elements in a `List`', () {
+        expect([2, 3, 5, -4].sum, 6);
       });
 
-      test('Can determine the maximum value in a `List`', (){
-        expect([2,3,5,-4].max, 5);
+      test('Can determine the maximum value in a `List`', () {
+        expect([2, 3, 5, -4].max, 5);
       });
 
-      test('Can determine the minimum value in a `List`', (){
-        expect([2,3,5,-4].min, -4);
+      test('Can determine the minimum value in a `List`', () {
+        expect([2, 3, 5, -4].min, -4);
       });
 
-      test('Can calculate the average of elements in a `List`', (){
-        expect([2,3,5,-4].average, 1.5);
+      test('Can calculate the average of elements in a `List`', () {
+        expect([2, 3, 5, -4].average, 1.5);
       });
 
-      test('Can not determine the minimum value in a `List` when it contain < 2 element', (){
+      test(
+          'Can not determine the minimum value in a `List` when it contain < 2 element',
+          () {
         expect(() => [2].min, throwsException);
       });
 
-      test('Can not determine the maximum value in a `List` when it contain < 2 element', (){
+      test(
+          'Can not determine the maximum value in a `List` when it contain < 2 element',
+          () {
         expect(() => [2].max, throwsException);
       });
 
     });
-
   });
 
 }
